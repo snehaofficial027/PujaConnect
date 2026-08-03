982071a35c353310577d8d58b563d924a2739583
Sneha Lalwani <sneha.official027@gmail.com>
2026-08-03 19:40:04 +0530

    Fix API import paths

--- client/src/pages/admin/AdminPandits.jsx
+++ client/src/pages/admin/AdminPandits.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import API from "../../api";
+import API from "../../config/api";
 import AdminLayout from "../../components/admin/AdminLayout";
 import {
   Eye,
--- client/src/pages/admin/AdminRevenue.jsx
+++ client/src/pages/admin/AdminRevenue.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import API from "../../api";
+import API from "../../config/api";
 import {
   IndianRupee,
   Calendar,
